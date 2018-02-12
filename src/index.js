08dc98c7f0cd97bd10126e53412be71af2371b13
devPavelM <MPavelMail@gmail.com>
2018-02-12 19:48:20 +0300

    add react-router, redux. main page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './main.css'
 
-document.write('Hello React/Redux!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {createStore, applyMiddleware} from 'redux';
+import {browserHistory} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Provider} from 'react-redux'
+import {Router, Route} from 'react-router'
+
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={Layout}>
+                <Route path='/' component={Phones} />
+            </Route>
+
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+)
